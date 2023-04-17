9baee9d259fbf6c2566f08d99c10d8b97ea49ed6
Dean-Hoong <76891624+Dean-Hoong@users.noreply.github.com>
2023-04-17 11:11:11 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -1,7 +1,6 @@
-import java.util.Scanner;
-
 public class Question2
 {
+  import java.util.Scanner;
   public static void main(String[] args)
   {
     System.out.print("Please enter you height in meters: ");
