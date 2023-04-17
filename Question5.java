c5a52151411f38e2241aa3e9ec166993074eaa31
Dean-Hoong <76891624+Dean-Hoong@users.noreply.github.com>
2023-04-17 14:32:30 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -4,29 +4,26 @@ public class Question5
 {
   public static void main(String[] args)
   {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
-    Scanner in = new Scanner(System.in);
-    
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter the number of integers: ");
+    int numIntegers = scanner.nextInt();
+    int[] freqArray = new int[1001];
+    for (int i = 0; i < numIntegers; i++) 
+        {
+        System.out.print("Enter an integer: ");
+        int num = scanner.nextInt();
+        freqArray[num]++;
+        }
+    int maxFreq = 0;
+    int mode = 0;
+    for (int i = 1; i <= 1000; i++) 
+        {
+        if (freqArray[i] > maxFreq) 
+            {
+            maxFreq = freqArray[i];
+            mode = i;
+            }
+        }
+        System.out.println(mode);
   }
 }
